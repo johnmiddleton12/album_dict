898cc1a029c1da1bf6f9162db81072c48c18717d
johnmiddleton12 <johnathanmiddleton1@gmail.com>
2020-07-28 14:08:17 -0400

    add basic structure of functions and structure of main script

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,6 @@
 # album dictionary
 
-albums = {'Artist 1':
+music = {'Artist 1':
               {'Album 1':
                    {'songs': ['song1', 'song2'],
                     'year': 1974,
@@ -10,10 +10,54 @@ albums = {'Artist 1':
           }
 #  TODO watch
 
+def add_artist():
+    pass
+
+def add_album():
+    pass
+
+def add_song():
+    pass
+
+def remove_artist():
+    pass
+
+def remove_album():
+    pass
+
+def remove_song():
+    pass
+
+def print_music():
+    # print all parts of 'music' dict
+    pass
+
+def edit_album():
+    # edit songs list, year, and platinum status
+    pass
+
+def menu():
+    # print menu options
+    # take input for option
+    # return input
+
 def main():
+
     var = input('Enter name:\n')
     print('Your name is {}'.format(var))
     print('Newport')
+
+    functions = [add_artist, add_album, add_song,
+                 remove_artist, remove_album, remove_song,
+                 print_music, edit_album]
+
+    var = 0
+
+    while var != 'exit':
+        var = menu()
+        functions[var]()
+
+
 
 #TODO do this here
 
