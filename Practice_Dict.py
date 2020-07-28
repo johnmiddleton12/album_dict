ab2c244a55e8e875831ee46bda6224c2083e7733
jtestwuide <67339169+jtestwuide@users.noreply.github.com>
2020-07-28 14:42:28 -0500

    La Flame

---
+++ Practice_Dict.py
@@ -0,0 +1,15 @@
+albums = {'Travis Scott':
+              {'Astroworld':
+                   {'songs': ['Stargazing', 'Carousel', 'Sicko Mode',
+                              'R.I.P. Screw', 'Stop Trying to Be God',
+                              'No Bystanders', 'Skeletons', 'Wake Up',
+                              '5% Tint', 'NC-17', 'Astrothunder', 'Yosemite',
+                              'Can’t Say', 'Who? What!', 'Butterfly Effect',
+                              'Houstonfornication', 'Coffee Bean'],
+                    'year': 2018,
+                    'platinum': True
+                    }
+               }
+          }
+inquiry = input('Enter Artist: ')
+print(albums[inquiry])
