5190c38ddf8f12b75ef029b72cda4277c83dcf96
johnmiddleton12 <johnathanmiddleton1@gmail.com>
2020-07-27 21:21:37 -0400

    Add main script

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,17 @@
 # album dictionary
 
-albums = {'Artist 1' :
-              {'Album 1' :
-                   { 'songs' : ['song1', 'song2'],
-                     'year' : 1974,
-                     'platinum' : True
-                   }
-              }
+albums = {'Artist 1':
+              {'Album 1':
+                   {'songs': ['song1', 'song2'],
+                    'year': 1974,
+                    'platinum': True
+                    }
+               }
           }
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
