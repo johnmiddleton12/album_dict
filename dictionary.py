9d4d13d87a35f095a7e4ab58d25104bc391d59b3
johnmiddleton12 <johnathanmiddleton1@gmail.com>
2020-07-28 14:10:33 -0400

    reformat

--- dictionary.py
+++ dictionary.py
@@ -1,48 +1,59 @@
 # album dictionary
 
 music = {'Artist 1':
-              {'Album 1':
-                   {'songs': ['song1', 'song2'],
-                    'year': 1974,
-                    'platinum': True
-                    }
-               }
-          }
+             {'Album 1':
+                  {'songs': ['song1', 'song2'],
+                   'year': 1974,
+                   'platinum': True
+                   }
+              }
+         }
+
+
 #  TODO watch
 
 def add_artist():
     pass
 
+
 def add_album():
     pass
+
 
 def add_song():
     pass
 
+
 def remove_artist():
     pass
+
 
 def remove_album():
     pass
 
+
 def remove_song():
     pass
+
 
 def print_music():
     # print all parts of 'music' dict
     pass
 
+
 def edit_album():
     # edit songs list, year, and platinum status
     pass
+
 
 def menu():
     # print menu options
     # take input for option
     # return input
+    pass
+
 
 def main():
-
     var = input('Enter name:\n')
     print('Your name is {}'.format(var))
     print('Newport')
@@ -58,8 +69,7 @@ def main():
         functions[var]()
 
 
-
-#TODO do this here
+# TODO do this here
 
 if __name__ == "__main__":
     main()
