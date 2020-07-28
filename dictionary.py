1f488ce456d57ea84f1c61281d9af1e131db3c79
johnmiddleton12 <johnathanmiddleton1@gmail.com>
2020-07-27 20:51:32 -0400

    initial dictionary add

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+# album dictionary
+
+albums = {'Artist 1' :
+              {'Album 1' :
+                   { 'songs' : ['song1', 'song2'],
+                     'year' : 1974,
+                     'platinum' : True
+                   }
+              }
+          }
